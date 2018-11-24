f053de2deaa782c1ba04d4c40d8343dee606b8cd
Emick L <esppk@outlook.com>
2018-11-25 05:25:14 +0800

    fix the stat

--- geom_hurrican.R
+++ geom_hurrican.R
@@ -1,15 +1,18 @@
 GeomHurricane <- ggproto("GeomHurricane", GeomPolygon,
-                   required_aes =c("x", "y", "r_ne", "r_se", "r_nw", "r_sw",
-                                   "fill", "color"),
-                   default_aes = aes(scale_radii = 1, alpha = 0.8)
+                   required_aes =c("x", "y", "ne", "se", "nw", "sw", "colour"),
+                   default_aes = aes(fill = NA, size = 0.5, linetype = 1,
+                                     alpha = NA)
 )
 
-geom_hurricane <- function(mapping = NULL, data = NULL, 
-                                        position = "identity", na.rm = FALSE, show.legend = NA, 
+geom_hurricane <- function(mapping = NULL, data = NULL,
+                                        position = "identity", na.rm = FALSE, show.legend = NA,
                                         inherit.aes = TRUE, ...) {
-    layer(
+    ggplot2::layer(
         stat = StatHurricane, geom = GeomHurricane, data = data, mapping = mapping,
         position = position, show.legend = show.legend, inherit.aes = inherit.aes,
         params = list(na.rm = na.rm, ...)
     )
 }
+
+
+
--- stat_hurrican.R
+++ stat_hurrican.R
@@ -1,41 +1,46 @@
 StatHurricane <- ggproto("StatHurricane", Stat,
-                        required_aes = c("x","y","r_ne","r_se","r_sw","r_nw","fill","color"),
-                        default_aes = aes(scale_radii = 1),
+
+                        #default_aes = aes(scale_radii = 1),
                         compute_group = function(data, scales){
-        
-                            bearing <- data.frame(dir = rep(c("r_ne","r_se","r_sw","r_nw"),each = 90), 
-                                bearing_ = c(seq(0,89), seq(90,179),
-                                        seq(180,269), seq(270,359)))
-                            
-                            dat_ <- data %>% 
-                                dplyr::select(fill,r_ne,r_se,r_sw,r_nw) %>% 
-                                tidyr::gather(dir_, dist_, -1) %>% 
-                                dplyr::mutate(dist_ = dist_*1852*scale_radii) %>% 
-                                tidyr::spread(fill, dist_) %>% 
-                                purrr::map_df(~rep(.x, each  = 90)) %>% 
-                                dplyr::bind_cols(bearing) %>% 
-                                dplyr::select(-5) %>% 
-                                tidyr::gather(fill, dist_, -dir_,-bearing_) %>% 
-                                dplyr::mutate(color = fill)
-                            
-                            points <- geosphere::destPoint(data[1,c("x","y")], dat_$bearing_, 
-                                                dat_$dist_)  %>% as.data.frame() %>% 
-                                bind_cols(dat_ %>% select(fill, color))  
+
+                            bearing <- data.frame(dir = rep(c("r_ne","r_se","r_sw","r_nw"), each = 90),
+                                                  bearing_ = c(seq(0,89), seq(90,179),
+                                                               seq(180,269), seq(270,359)))
+
+                            dat_ <- data %>%
+                                dplyr::rename(wind_speed = fill) %>%
+                                dplyr::select(wind_speed, ne,se,sw,nw) %>%
+                                tidyr::gather(dir_, dist_, -wind_speed) %>%
+                                dplyr::mutate(dist_ = dist_*1852*1) %>%
+                                tidyr::spread(wind_speed, dist_) %>%
+                                purrr::map_df(~rep(.x, each  = 90)) %>%
+                                dplyr::bind_cols(bearing) %>%
+                                dplyr::select(-dir) %>%
+                                tidyr::gather(wind_speed, dist_, -dir_,-bearing_)
+
+                            points <- geosphere::destPoint(c(23.1, -75.1), dat_$bearing_,
+                                                           dat_$dist_)  %>% as.data.frame() %>%
+                                bind_cols(dat_ %>% select(wind_speed))
+
+                            #data.frame(x = points$lon, y = points$lat, fill = points$wind_speed)
+                            #data.frame(x = dist, y = 8, colour = data$speed)
+                            names(points) <- c("x", "y", "fill")
                             points
 
-                        }
+                        },
+                        required_aes = c("ne", "nw", "se", "sw")
 )
 
 stat_hurricane <- function(mapping = NULL, data = NULL, geom = "polygon",
-                                          position = "identity", na.rm = FALSE, 
+                                          position = "identity", na.rm = FALSE,
                                           show.legend = NA, inherit.aes = TRUE,...) {
     ggplot2::layer(
-        stat = StatHurricane, 
-        data = data, 
-        mapping = mapping, 
-        geom = geom, 
-        position = position, 
-        show.legend = show.legend, 
+        stat = StatHurricane,
+        data = data,
+        mapping = mapping,
+        geom = geom,
+        position = position,
+        show.legend = show.legend,
         inherit.aes = inherit.aes,
         params = list(na.rm = na.rm, ...)
     )
